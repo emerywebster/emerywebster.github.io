e804c759091167352e6cebbcb8eda8a3ae83b4e8
Webb Henderson <webb.henderson@gmail.com>
2020-10-10 16:09:05 -0600

    Add :visited workaround

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,16 @@ function toggleMenu() {
 	var menu = document.getElementById('mobileMenu');
 	menu.classList.toggle('hidden');
 }
+
+// http://joelcalifa.com/blog/revisiting-visited
+localStorage.setItem('visited-' + window.location.pathname, true);
+var links = document.getElementsByTagName('a');
+for (i = 0; i < links.length; i++) {
+	var link = links[i];
+	if (
+		(link.host == window.location.host && localStorage.getItem('visited-' + link.pathname)) ||
+		localStorage.getItem('visited-' + link.pathname + '/')
+	) {
+		link.dataset.visited = true;
+	}
+}
