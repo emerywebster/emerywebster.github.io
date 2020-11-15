08a7deb76a36446818c2991f5acffb0fb39d6a84
Webb Henderson <webb.henderson@gmail.com>
2020-11-15 13:25:29 -0700

    Add typing effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Mobile menu
 function toggleMenu() {
 	var icon = document.getElementById('menuIcon');
 	icon.classList.toggle('fa-bars');
@@ -20,9 +21,52 @@ for (i = 0; i < links.length; i++) {
 	}
 }
 
+// Dark mode
 const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
 if (prefersDarkScheme.matches) {
 	document.body.classList.add('dark-theme');
 } else {
 	document.body.classList.remove('dark-theme');
 }
+
+// Type effect
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".typed-cursor");
+const textArray = ["interfaces.", "systems.", "experiences.", "products."];
+const typingDelay = 100;
+const erasingDelay = 75;
+const newTextDelay = 750; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 75);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 100);
+});
